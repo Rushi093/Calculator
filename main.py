eed484ba77731c1a6320eec61158e4ddb08c4b38
Rushi093 <143588163+Rushi093@users.noreply.github.com>
2026-01-02 23:02:11 +0530

    Remove unnecessary git commands from main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-git branch -m main <BRANCH>
-git fetch origin
-git branch -u origin/<BRANCH> <BRANCH>
-git remote set-head origin -a
 from mean_var_std import calculate
 from test_module import TestMeanVarStd
 import unittest
@@ -12,4 +8,5 @@ print(calculate([0,1,2,3,4,5,6,7,8]))
 # Run unit tests
 if __name__ == "__main__":
     unittest.main()
+
 
