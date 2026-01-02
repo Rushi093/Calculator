97262c23442ad87f81a24777244b04b328d0a48e
Rushi093 <143588163+Rushi093@users.noreply.github.com>
2026-01-02 23:01:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+git branch -m main <BRANCH>
+git fetch origin
+git branch -u origin/<BRANCH> <BRANCH>
+git remote set-head origin -a
 from mean_var_std import calculate
 from test_module import TestMeanVarStd
 import unittest
@@ -8,3 +12,4 @@ print(calculate([0,1,2,3,4,5,6,7,8]))
 # Run unit tests
 if __name__ == "__main__":
     unittest.main()
+
